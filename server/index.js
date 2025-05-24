deada7dec9912074e31f3f1ef0623aaf8adc20de
Mohamed Hamas <e0726186@u.nus.edu>
2025-05-24 18:01:56 +0800

    add port

--- server/index.js
+++ server/index.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use('/api', userRoutes); 
 app.use('/api', audioRoutes);
   
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log("Server has started on port 3001");
   });
